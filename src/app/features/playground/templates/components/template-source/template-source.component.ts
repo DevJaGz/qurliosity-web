99a6b6e56f1110ef5ef7ed39514327cfb053ff8a
DevJaGz <jgomezpersonal7@gmail.com>
2024-05-22 21:44:32 -0500

    feat: show pdf icon

--- src/app/features/playground/templates/components/template-source/template-source.component.ts
+++ src/app/features/playground/templates/components/template-source/template-source.component.ts
@@ -1,19 +1,24 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  Input,
+  computed,
   input,
 } from '@angular/core';
 import { Source } from '@core/datatypes';
+import { SourceType } from '@core/enums';
+import { SharedModule } from '@shared/index';
 
 @Component({
   selector: 'app-template-source',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './template-source.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TemplateSourceComponent {
   source = input.required<Source>();
+  type = SourceType;
+  sourceType = computed(() => this.source().type);
+  sourceName = computed(() => this.source().value);
 }
--- src/app/shared/constants/icons.constant.ts
+++ src/app/shared/constants/icons.constant.ts
@@ -2,12 +2,14 @@ import {
   IconCamera,
   IconHeart,
   IconBrandGithub,
+  IconFileTypePdf,
 } from 'angular-tabler-icons/icons';
 
 const icons = {
   IconCamera,
   IconHeart,
   IconBrandGithub,
+  IconFileTypePdf,
 } as const;
 
 export { icons as TABLER_ICONS };
