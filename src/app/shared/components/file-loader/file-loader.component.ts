0d132da29a72c9c3aee296e0f10699042c44a72a
DevJaGz <jgomezpersonal7@gmail.com>
2024-05-24 22:05:27 -0500

    refactor: use outputFromObservable for is over drag zone logic

--- src/app/shared/components/file-loader/file-loader.component.ts
+++ src/app/shared/components/file-loader/file-loader.component.ts
@@ -2,15 +2,16 @@ import { AsyncPipe, NgClass } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
-  EventEmitter,
   input,
   output,
 } from '@angular/core';
+import { outputFromObservable } from '@angular/core/rxjs-interop';
 import { SharedModule } from '@shared/shared.module';
 import { cancelEvent } from '@shared/utils';
 import { FileLoaderService } from './services';
 import { UploadedFiles } from '@shared/datatypes';
 import { DragzoneDirective } from '@shared/directives/dragzone.directive';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-file-loader',
@@ -25,8 +26,10 @@ export class FileLoaderComponent {
   disabled = input<boolean | undefined>(false);
   multiple = input<boolean | undefined>(false);
   accept = input<string | undefined>('*');
-  isOverDragZone = new EventEmitter<boolean>();
   files = output<UploadedFiles>();
+  readonly #isOverDragZone = new Subject<boolean>();
+  isOverDragZone$ = this.#isOverDragZone.asObservable();
+  isOverDragZone = outputFromObservable<boolean>(this.isOverDragZone$);
 
   attached(event: Event): void {
     cancelEvent(event);
@@ -38,11 +41,15 @@ export class FileLoaderComponent {
 
   onDragZoneEvent(event: DragEvent): void {
     if (['dragenter', 'dragover'].includes(event.type)) {
-      this.isOverDragZone.emit(true);
+      this.#isOverDragZone.next(true);
     }
 
     if (['dragleave', 'drop'].includes(event.type)) {
-      this.isOverDragZone.emit(false);
+      this.#isOverDragZone.next(false);
+    }
+
+    if (event.type === 'dragover') {
+      event.dataTransfer ? (event.dataTransfer.dropEffect = 'copy') : null;
     }
 
     if (event.type === 'drop') {
