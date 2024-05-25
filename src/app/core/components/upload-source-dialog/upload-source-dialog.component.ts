57dbe0a1878b57c875f55ccd83c103231db1864e
DevJaGz <jgomezpersonal7@gmail.com>
2024-05-25 11:41:01 -0500

    feat: avoid duplicated uploads in source dialog

--- src/app/core/components/upload-source-dialog/upload-source-dialog.component.ts
+++ src/app/core/components/upload-source-dialog/upload-source-dialog.component.ts
@@ -2,11 +2,13 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
+  inject,
   signal,
 } from '@angular/core';
 import { FileLoaderComponent } from '@shared/components';
-import { UploadedFiles } from '@shared/datatypes';
+import { UploadedFile, UploadedFiles } from '@shared/datatypes';
 import { SharedModule } from '@shared/shared.module';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-upload-source-dialog',
@@ -17,6 +19,7 @@ import { SharedModule } from '@shared/shared.module';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UploadSourceDialogComponent {
+  readonly #messageService = inject(MessageService);
   readonly uploadedFiles = signal<UploadedFiles>([]);
   #numberOfUploadedFiles = computed(() => this.uploadedFiles().length);
   readonly maxFileSize = 1024 * 1024 * 10;
@@ -24,6 +27,43 @@ export class UploadSourceDialogComponent {
 
   onUploadedFiles(uploadedFiles: UploadedFiles): void {
     console.log(uploadedFiles);
-    this.uploadedFiles.update((files) => [...files, ...uploadedFiles]);
+
+    const duplicatedFiles = this.#getDuplicatedFiles(uploadedFiles);
+    if (duplicatedFiles.length > 0) {
+      this.#notifyDuplicatedFiles(duplicatedFiles);
+      return;
+    }
+
+    this.uploadedFiles.update((currentUploadedFiles) => [
+      ...currentUploadedFiles,
+      ...uploadedFiles,
+    ]);
+  }
+
+  #getDuplicatedFiles(uploadedFiles: UploadedFiles): UploadedFiles {
+    const duplicatedFiles: UploadedFiles = [];
+    for (const uploadedFile of uploadedFiles) {
+      const duplicatedFile = this.#getDuplicatedFile(uploadedFile.file);
+      if (duplicatedFile) {
+        duplicatedFiles.push(duplicatedFile);
+      }
+    }
+    return duplicatedFiles;
+  }
+
+  #getDuplicatedFile(file: File): UploadedFile | null {
+    const currentUploadedFiles = this.uploadedFiles();
+    return currentUploadedFiles.find((f) => f.file.name === file.name) || null;
+  }
+
+  #notifyDuplicatedFiles(duplicatedFiles: UploadedFiles): void {
+    for (const duplicatedFile of duplicatedFiles) {
+      this.#messageService.add({
+        severity: 'warn',
+        summary: 'Duplicated files',
+        detail: `The following file was already uploaded: ${duplicatedFile.file.name}`,
+        life: 10000,
+      });
+    }
   }
 }
