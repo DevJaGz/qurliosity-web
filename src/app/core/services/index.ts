088c8405002f8a5b1df684c34ffc6742516c3535
DevJaGz <jgomezpersonal7@gmail.com>
2024-05-26 11:38:56 -0500

    refactor: remove app state service

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -1,7 +1,6 @@
 export * from './templates';
 export * from './dialogs';
 export * from './app-error.service';
-export * from './app-state.service';
 export * from './loader-indicator.service';
 export * from './files';
 export * from './upload-source.service';
