0d78968f13552f7cd2facf32e3f150244bf859a6
DevJaGz <jgomezpersonal7@gmail.com>
2024-05-30 11:38:37 -0500

    chor: create prompts playground to test signals

--- src/app/features/playground/templates/components/template-prompts/template-prompts.component.ts
+++ src/app/features/playground/templates/components/template-prompts/template-prompts.component.ts
@@ -1,13 +1,45 @@
+import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-template-prompts',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './template-prompts.component.html',
   styles: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TemplatePromptsComponent {
+  form = new FormGroup({
+    name: new FormControl(''),
+    items: new FormArray([]),
+  });
 
+  items = this.form.get('items') as FormArray;
+
+  xItemsFormControls = toSignal(
+    this.items.valueChanges.pipe(map(() => [...this.items.controls])),
+    {
+      initialValue: [],
+    }
+  );
+
+  add(name = 'item') {
+    setTimeout(() => {
+      this.items.push(
+        new FormGroup({
+          name: new FormControl(name),
+        })
+      );
+    }, 2000);
+  }
+
+  constructor() {
+    this.items.valueChanges.subscribe((items) => {
+      console.log('CTOR: items', items);
+    });
+  }
 }
