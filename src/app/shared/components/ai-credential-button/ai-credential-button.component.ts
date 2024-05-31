1b9f4641daffe28800d8818aa87c78d48e88d472
DevJaGz <jgomezpersonal7@gmail.com>
2024-05-30 20:59:11 -0500

    style: set ai credential button colors based on state

--- src/app/shared/components/ai-credential-button/ai-credential-button.component.ts
+++ src/app/shared/components/ai-credential-button/ai-credential-button.component.ts
@@ -1,12 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 
 @Component({
   selector: 'app-ai-credential-button',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, NgClass],
   templateUrl: './ai-credential-button.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AiCredentialButtonComponent {}
+export class AiCredentialButtonComponent {
+  isValid = input.required<boolean>();
+  dissabled = input.required<boolean>();
+}
