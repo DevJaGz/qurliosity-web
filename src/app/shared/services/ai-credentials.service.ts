a8fa5f4934e670f03c15b3d3617d95c1a68303e5
DevJaGz <jgomezpersonal7@gmail.com>
2024-05-30 21:39:29 -0500

    feat: allow add embedder credentials using ai credential service

--- src/app/shared/services/ai-credentials.service.ts
+++ src/app/shared/services/ai-credentials.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, computed, inject, signal } from '@angular/core';
-import { AICredentials } from '@core/datatypes';
+import { AICredentials, EmbedderCredential } from '@core/datatypes';
 import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
@@ -16,6 +16,17 @@ export class AiCredentialsService {
   readonly hasAICredentials = computed(() => {
     return Boolean(this.#AICredentials().embedderCredential);
   });
+
+  setEmbedderCredential(embedderCredential: EmbedderCredential) {
+    this.#AICredentials.set({
+      embedderCredential,
+    });
+    this.#upadteLocalStorage();
+  }
+
+  #upadteLocalStorage() {
+    this.#localStorageService.setItem(this.#STORAGE_KEY, this.#AICredentials());
+  }
 
   constructor() {
     const AICredentials = this.#localStorageService.getItem(
