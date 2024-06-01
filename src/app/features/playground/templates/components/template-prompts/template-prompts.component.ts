f5c1821e280cf3d373f6c6b24e69faa105b605ae
DevJaGz <jgomezpersonal7@gmail.com>
2024-05-31 20:06:07 -0500

    style: set same padding for main template route outlet

--- src/app/features/playground/templates/components/template-prompts/template-prompts.component.ts
+++ src/app/features/playground/templates/components/template-prompts/template-prompts.component.ts
@@ -8,58 +8,9 @@ import { computedFormControls, toSignalFormArray } from '@shared/utils';
 @Component({
   selector: 'app-template-prompts',
   standalone: true,
-  imports: [JsonPipe],
+  imports: [],
   templateUrl: './template-prompts.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TemplatePromptsComponent {
-  form = new FormGroup({
-    name: new FormControl(''),
-    items: new FormArray([]),
-  });
-
-  get value$() {
-    return this.form.valueChanges;
-  }
-
-  #itemsArray = this.form.get('items') as FormArray;
-  // itemsControls = this.#itemsArray.controls;
-  get itemsControls() {
-    return this.#itemsArray.controls;
-  }
-
-  xItemsArray = toSignalFormArray(this.#itemsArray);
-
-  xItemsControls = computedFormControls(this.xItemsArray);
-
-  add(name = 'item') {
-    setTimeout(() => {
-      const randomHashName = Math.random().toString(36).substring(2);
-      this.#itemsArray.push(
-        new FormGroup({
-          arrayItemName: new FormControl(name + randomHashName),
-        })
-      );
-    }, 2000);
-  }
-
-  delete() {
-    setTimeout(() => {
-      this.#itemsArray.clear();
-    }, 2000);
-  }
-
-  deleteOne(index: number) {
-    setTimeout(() => {
-      this.#itemsArray.removeAt(index);
-    }, 2000);
-  }
-
-  constructor() {
-    console.log('CTOR', this.form, this.#itemsArray);
-    this.value$.subscribe((items) => {
-      console.log('CTOR: items', items);
-    });
-  }
-}
+export class TemplatePromptsComponent {}
