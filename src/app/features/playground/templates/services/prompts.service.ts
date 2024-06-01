6364d067a3e57598349a5142a9c6caa1207d5443
DevJaGz <jgomezpersonal7@gmail.com>
2024-06-01 14:32:41 -0500

    feat: create prompt controls signals in prompt service

--- src/app/features/playground/templates/services/prompts.service.ts
+++ src/app/features/playground/templates/services/prompts.service.ts
@@ -1,10 +1,17 @@
 import { Injectable, inject } from '@angular/core';
 import { TemplateFormService } from './template-form.service';
+import { PromptsRequestService } from '@core/services';
+import { computedFormControls, toSignalFormArray } from '@shared/utils';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PromptsService {
   readonly #templateFormService = inject(TemplateFormService);
+  readonly #promptsRequestService = inject(PromptsRequestService);
   readonly templateId = this.#templateFormService.templateId;
+  readonly promptsFormArray = toSignalFormArray(
+    this.#templateFormService.promptFormArray
+  );
+  readonly promptsFormControls = computedFormControls(this.promptsFormArray);
 }
